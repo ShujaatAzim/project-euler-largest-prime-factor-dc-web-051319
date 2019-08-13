3b78633e191624ac14d804fadac2b87515b03155
Shujaat Azim <shujaat.azim@gmail.com>
2019-08-13 14:19:40 -0400

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,25 @@
-# Enter your procedural solution here!
+# Enter your procedural solution here
+
+def largest_prime_factor(input)
+
+    new_number = Integer.sqrt(input).round
+    factors = []
+    primes = []
+
+    for num in (2..new_number) 
+      if input % num == 0
+        factors.push(num)
+      end
+    end
+    
+    factors.each do |num|
+      if (1..num).select { |d| num % d == 0 } == [1, num]
+        primes.push(num)
+      end
+    end
+    
+  return primes.max
+
+end
+
+
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,30 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+
+    def initialize(num)
+        @num = num
+    end
+
+    def number
+        new_number = Integer.sqrt(@num).round
+        factors = []
+        primes = []
+    
+        for num in (2..new_number) 
+          if @num % num == 0
+            factors.push(num)
+          end
+        end
+        
+        factors.each do |num|
+          if (1..num).select { |d| num % d == 0 } == [1, num]
+            primes.push(num)
+          end
+        end
+        
+      return primes.max
+    
+    end
+
+
+end
